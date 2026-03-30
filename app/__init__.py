83be9357d4b97f745b02fe12ea0557b9185a8393
Naomi Kurian <nkurian60@stuy.edu>
2026-03-30 11:17:51 -0400

    cw

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,27 @@
+import sqlite3
+import random
+from flask import Flask, render_template
+from flask import session, request, redirect
 
+# Flask
+app = Flask(__name__)
+app.secret_key = 'secretkey'
+
+# SQLite
+DB_FILE = "data.db"
+
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()
+
+db.commit()
+db.close()
+
+@app.route('/', methods=["GET", "POST"])
+def homepage():
+    return render_template("home.html")
+
+
+# Flask
+if __name__=='__main__':
+    app.debug = False
+    app.run()
